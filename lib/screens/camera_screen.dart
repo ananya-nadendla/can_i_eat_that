a32e3cb0217a6772a025d2dcd72b11accca6f911
ananya-nadendla <ananya.nadendla@gmail.com>
2024-08-29 11:56:07 -0400

    v0.9.9 - removed back navigation

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -77,8 +77,8 @@ class _CameraScreenState extends State<CameraScreen> {
           ),
           TextButton(
             onPressed: () {
-                Navigator.of(context).pushAndRemoveUntil(
-                MaterialPageRoute(builder: (context) => HomeScreen()), //when user hits "Ok", send back to HomeScreen
+              Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (context) => HomeScreen()), // When user hits "Ok", send back to HomeScreen
                 (route) => false,
               );
             },
@@ -113,6 +113,11 @@ class _CameraScreenState extends State<CameraScreen> {
     Navigator.of(context).pop(null); // Return null to indicate cancellation
   }
 
+  Future<bool> _onWillPop() async {
+    // Handle the back navigation
+    return false; // Returning false disables the back navigation
+  }
+
   @override
   Widget build(BuildContext context) {
     // Fetch screen dimensions
@@ -130,103 +135,107 @@ class _CameraScreenState extends State<CameraScreen> {
       });
     }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Capture Photo'),
-      ),
-      body: Column(
-        children: [
-          // Camera Preview Section
-          Expanded(
-            child: Stack(
-              children: [
-                FutureBuilder<void>(
-                  future: _initializeControllerFuture,
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.done) {
-                      if (_controller != null && _controller!.value.isInitialized) {
-                        return _capturedImage == null
-                            ? CameraPreview(_controller!)
-                            : Image.file(_capturedImage!);
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Capture Photo'),
+          automaticallyImplyLeading: false, // Remove the back arrow
+        ),
+        body: Column(
+          children: [
+            // Camera Preview Section
+            Expanded(
+              child: Stack(
+                children: [
+                  FutureBuilder<void>(
+                    future: _initializeControllerFuture,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        if (_controller != null && _controller!.value.isInitialized) {
+                          return _capturedImage == null
+                              ? CameraPreview(_controller!)
+                              : Image.file(_capturedImage!);
+                        } else {
+                          return const Center(child: Text('Camera not initialized'));
+                        }
+                      } else if (snapshot.hasError) {
+                        return Center(child: Text('Error: ${snapshot.error}'));
                       } else {
-                        return const Center(child: Text('Camera not initialized'));
+                        return const Center(child: CircularProgressIndicator());
                       }
-                    } else if (snapshot.hasError) {
-                      return Center(child: Text('Error: ${snapshot.error}'));
-                    } else {
-                      return const Center(child: CircularProgressIndicator());
-                    }
-                  },
-                ),
-                
-                // Camera Buttons Section
-                if (_capturedImage == null)
-                  Positioned(
-                    bottom: bottomPadding,
-                    left: 0,
-                    right: 0,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        GestureDetector(
-                          onTap: _captureImage,
-                          child: Container(
-                            width: buttonSize,
-                            height: buttonSize,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.white,
-                              border: Border.all(width: buttonSize * 0.07, color: Colors.black), // 7% of button size
+                    },
+                  ),
+                  
+                  // Camera Buttons Section
+                  if (_capturedImage == null)
+                    Positioned(
+                      bottom: bottomPadding,
+                      left: 0,
+                      right: 0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          GestureDetector(
+                            onTap: _captureImage,
+                            child: Container(
+                              width: buttonSize,
+                              height: buttonSize,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white,
+                                border: Border.all(width: buttonSize * 0.07, color: Colors.black), // 7% of button size
+                              ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
+                    )
+                  else
+                    Positioned(
+                      bottom: bottomPadding,
+                      left: 0,
+                      right: 0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.close, color: Colors.red),
+                            iconSize: buttonSize,
+                            onPressed: _cancelCameraCapture, // Return null instead of navigating to HomeScreen
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.refresh, color: Colors.blue),
+                            iconSize: buttonSize,
+                            onPressed: _resetCamera,
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.check, color: Colors.green),
+                            iconSize: buttonSize,
+                            onPressed: () {
+                              Navigator.of(context).pop(_capturedImage); // Return the captured image
+                            },
+                          ),
+                        ],
+                      ),
                     ),
-                  )
-                else
-                  Positioned(
-                    bottom: bottomPadding,
-                    left: 0,
-                    right: 0,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.close, color: Colors.red),
-                          iconSize: buttonSize,
-                          onPressed: _cancelCameraCapture, // Return null instead of navigating to HomeScreen
-                        ),
-                        IconButton(
-                          icon: const Icon(Icons.refresh, color: Colors.blue),
-                          iconSize: buttonSize,
-                          onPressed: _resetCamera,
-                        ),
-                        IconButton(
-                          icon: const Icon(Icons.check, color: Colors.green),
-                          iconSize: buttonSize,
-                          onPressed: () {
-                            Navigator.of(context).pop(_capturedImage); // Return the captured image
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-              ],
+                ],
+              ),
             ),
-          ),
-          
-          // Camera Message Section
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: screenHeight * 0.02), // 2% of screen height
-            child: const Text(
-              'Take a photo of an ingredients label. \n Up Next: Crop your photo',
-              style: TextStyle(color: Colors.black),
-              softWrap: true,
-              overflow: TextOverflow.visible,
-              textAlign: TextAlign.center,
+            
+            // Camera Message Section
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: screenHeight * 0.02), // 2% of screen height
+              child: const Text(
+                'Take a photo of an ingredients label. \n Up Next: Crop your photo',
+                style: TextStyle(color: Colors.black),
+                softWrap: true,
+                overflow: TextOverflow.visible,
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/screens/crop_image_screen.dart
+++ lib/screens/crop_image_screen.dart
@@ -27,53 +27,60 @@ class _CropScreenState extends State<CropScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Crop Photo'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(false); // Indicate cropping was canceled
-            },
-            child: const Text('Cancel'),
-          ),
-          TextButton(
-            onPressed: _isCropping
-                ? null // Disable button while cropping is in progress (to avoid double tap)
-                : () {
-                    setState(() {
-                      _isCropping = true; // Set flag to true
-                    });
-                    _cropController.crop(); // Trigger cropping
+    return WillPopScope(
+      onWillPop: () async {
+        // Handle the back navigation
+        return false; // Returning false disables the back navigation
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Crop Photo'),
+          automaticallyImplyLeading: false, // Remove the back arrow
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false); // Indicate cropping was canceled
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: _isCropping
+                  ? null // Disable button while cropping is in progress (to avoid double tap)
+                  : () {
+                      setState(() {
+                        _isCropping = true; // Set flag to true
+                      });
+                      _cropController.crop(); // Trigger cropping
+                    },
+              child: const Text('Continue'),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0), // Add padding to avoid the edges - prevents back gesture on some phones
+                child: Crop(
+                  image: File(widget.imageFile.path).readAsBytesSync(),
+                  controller: _cropController,
+                  onCropped: (croppedData) {
+                    Navigator.of(context).pop(croppedData); // Return the cropped data
                   },
-            child: const Text('Continue'),
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0), // Add padding to avoid the edges - prevents back gesture on some phones
-              child: Crop(
-                image: File(widget.imageFile.path).readAsBytesSync(),
-                controller: _cropController,
-                onCropped: (croppedData) {
-                  Navigator.of(context).pop(croppedData); // Return the cropped data
-                },
+                ),
               ),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(16),
-            child: Text(
-              'Crop the image to include only the ingredients you wish to scan.',
-              style: TextStyle(color: Colors.black),
-              softWrap: true,
-              overflow: TextOverflow.visible,
+            const Padding(
+              padding: EdgeInsets.all(16),
+              child: Text(
+                'Crop the image to include only the ingredients you wish to scan.',
+                style: TextStyle(color: Colors.black),
+                softWrap: true,
+                overflow: TextOverflow.visible,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
